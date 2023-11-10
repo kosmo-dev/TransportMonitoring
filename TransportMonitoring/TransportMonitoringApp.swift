0f5b015c3fe1a454a05305132a207f3aad3a8101
Vadim Kuzmin <kuzminvadima@yandex.ru>
2023-11-10 16:11:48 +0300

    Initial Commit

---
+++ TransportMonitoring/TransportMonitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransportMonitoringApp.swift
+//  TransportMonitoring
+//
+//  Created by Вадим Кузьмин on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TransportMonitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
